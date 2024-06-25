f5fdf595ec5e587dd21a149d545753c8a6f04097
choiiw <tjddnjs5245@naver.com>
2024-06-25 22:49:36 +0900

    hh

--- src/ch16/App.js
+++ src/ch16/App.js
@@ -3,76 +3,96 @@ import "./App.css";
 ;
 
 function App() {
-
     const emptyUser = {
         name: "",
         email: ""
-    }
+    };
 
-    const [ user, setUser ] = useState({...emptyUser});
-    const [ inputData, setInputData ] = useState({ ...emptyUser });
-    const [ imgSrc, setImgSrc ] = useState("");
+    const [user, setUser] = useState({ ...emptyUser });
+    const [inputData, setInputData] = useState({ ...emptyUser });
+    const [imgSrc, setImgSrc] = useState("");
 
     useEffect(() => {
         const lsUser = localStorage.getItem("user");
-        setUser(!lsUser ? [] : JSON.parse(lsUser));
+        const lsImgSrc = localStorage.getItem("imgSrc");
+
+        if (lsUser) {
+            setUser(JSON.parse(lsUser));
+            setInputData(JSON.parse(lsUser));
+        }
+        if (lsImgSrc) {
+            setImgSrc(lsImgSrc);
+        }
     }, []);
 
     useEffect(() => {
         localStorage.setItem("user", JSON.stringify(user));
     }, [user]);
 
+    useEffect(() => {
+        localStorage.setItem("imgSrc", imgSrc);
+    }, [imgSrc]);
+
     const handleImgClick = () => {
-                const fileElement = document.createElement("input");
-                fileElement.setAttribute("type", "file");
-                fileElement.click();        
-                fileElement.onchange = (e) => {
-                    const file = e.target.files[0];
-                    const fileReader = new FileReader();
-                    fileReader.onload = (e) => {
-                        setImgSrc(e.target.result);
-                    }        
-                    fileReader.readAsDataURL(file);
-                }
+        const fileElement = document.createElement("input");
+        fileElement.setAttribute("type", "file");
+        fileElement.click();
+        fileElement.onchange = (e) => {
+            const file = e.target.files[0];
+            const fileReader = new FileReader();
+            fileReader.onload = (e) => {
+                setImgSrc(e.target.result);
             };
+            fileReader.readAsDataURL(file);
+        };
+    };
 
     const handleInputChange = (e) => {
-        setInputData(inputData => ({
+        setInputData((inputData) => ({
             ...inputData,
             [e.target.name]: e.target.value
-        })
-    )}
-    
-    const handleOkClick = (e) => {
+        }));
+    };
+
+    const handleOkClick = () => {
         setUser(inputData);
     };
 
-    
-    return ( 
+    return (
         <>
             <div className="out-box">
                 <div className="container">
                     <div className="logo-box">
                         <h1>프로필</h1>
-                    </div>                   
+                    </div>
                     <div className="img-box" onClick={handleImgClick}>
-                        <img src={imgSrc} />
-                    </div>   
+                        <img src={imgSrc} alt="프로필 이미지" />
+                    </div>
                     <div className="input-box">
                         <h3>이름</h3>
-                       <input type="text" name="name" onChange={handleInputChange} value={inputData.name}/>
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={handleInputChange}
+                            value={inputData.name}
+                        />
                     </div>
                     <div className="input-box">
                         <h3>이메일</h3>
-                        <input type="text" name="email" onChange={handleInputChange} value={inputData.email}/>
+                        <input
+                            type="text"
+                            name="email"
+                            onChange={handleInputChange}
+                            value={inputData.email}
+                        />
                     </div>
                     <div className="bt-box">
-                    <button onClick={handleOkClick}>저장</button>
+                        <button onClick={handleOkClick}>저장</button>
                     </div>
                 </div>
             </div>
         </>
-     );
+    );
 }
 
 export default App;
